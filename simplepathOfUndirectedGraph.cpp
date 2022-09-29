5f143f4230fd3cc5115cebb699fa6eb541781410
Muhammad  Sagor <70064576+Sagor0078@users.noreply.github.com>
2022-09-29 11:43:19 +0600

    Add files via upload

---
+++ simplepathOfUndirectedGraph.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define pb push_back
+
+const int mxN = 2e5+10;
+vector<ll> adj[mxN];
+bool visited[mxN];
+vector<ll> st;
+void dfs(int x,int y){
+    st.pb(x);
+    if(x == y){
+    	for(int i=0;i<st.size();i++){
+    		cout<<st[i]<<" ";
+    	}
+    	cout<<"\n";
+    	return;
+    }
+    visited[x] = true;
+    if(!adj[x].empty()){
+    	for(int j=0;j<adj[x].size();j++){
+    	    if(visited[adj[x][j]] == false){
+    	    	dfs(adj[x][j],y);
+    	    }	
+    	}
+    }
+    st.pop_back();
+}
+int main(){
+	int N,u,v;cin>>N>>u>>v;
+	N--;
+	for(int i=0;i<N;++i){
+		int a,b;cin>>a>>b;
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+	dfs(u,v);
+	return 0;
+}
